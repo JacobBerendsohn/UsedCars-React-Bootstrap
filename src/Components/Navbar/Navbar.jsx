82c7fd1ca472428ebf08898fdda345a1fb1dd23f
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-09-06 20:49:34 -0400

    Added navbar components

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,12 +5,22 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 const Navbar = () => {
   return (
     <div>
-        <nav class='navbar navbar-expand-lg bg-dark navbar-dark'>
-            <div class="container">
-                <a href='#' class='navbar-brand'>Value Inc</a>
+        <nav className='navbar navbar-expand-lg bg-dark navbar-dark'>
+            <div className="container">
+                <a href='#' className='navbar-brand'>Value Inc</a>
 
                 <div className="collapse navbar-collapse">
-                    
+                    <ul className='navbar-nav'>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Directions</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Hours</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">Contact Us</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </nav>
