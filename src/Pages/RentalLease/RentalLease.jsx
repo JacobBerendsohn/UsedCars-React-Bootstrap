412dcbfab8abae19e9bfa960647cccc9099f0ae2
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-09-09 10:55:33 -0400

    Added pages for long term rental button and inventory button

---
+++ src/Pages/RentalLease/RentalLease.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './RentalLease.css'
+
+const RentalLease = () => {
+  return (
+    <div>
+        Long Term Rental, Short Term Lease
+    </div>
+  )
+}
+
+export default RentalLease
