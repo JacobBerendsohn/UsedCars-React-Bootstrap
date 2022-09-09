d42647c6dfc7f396bc2c55f771f7dd76659a20ce
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-09-09 10:40:27 -0400

    Added site logo to head

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Pages/Home/Home.jsx';
-import About from './Pages/About/About.jsx';
 import Navbar from './Components/Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
@@ -10,12 +9,10 @@ import logo from './Assets/images/logo-with-silver-effect.png'
 function App() {
   return (
     <div>
-      {/** <img src={logo} alt="" /> */}
       <Navbar />
       <Header />
       <Routes>
           <Route path="/" element={ <Home /> } />
-          <Route path="/about" element={ <About /> } />
       </Routes>
       <Footer />
     </div>
