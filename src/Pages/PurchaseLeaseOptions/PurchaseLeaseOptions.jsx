cb8b4668184753fb4df3b7f827558f76d444ce4b
Jacob Berendsohn <Jacob.Berendsohn1@marist.edu>
2022-09-09 10:52:48 -0400

    Added pages for What makes us unique button and Purchase/Lease Options button

---
+++ src/Pages/PurchaseLeaseOptions/PurchaseLeaseOptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PurchaseLeaseOptions.css'
+
+const PurchaseLeaseOptions = () => {
+  return (
+    <div>
+        Purchase/Lease Options
+    </div>
+  )
+}
+
+export default PurchaseLeaseOptions
---
+++ src/Pages/Unique/Unique.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Unique.css'
+
+const Unique = () => {
+  return (
+    <div>
+        Unique
+    </div>
+  )
+}
+
+export default Unique
